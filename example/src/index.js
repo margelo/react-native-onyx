7fc291e7314125d04872916b5b7b64e76bd92690
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-08-02 16:55:58 +0200

    add react to example

--- example/src/index.js
+++ example/src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
 import Onyx from '../../lib/index';
+
+function App() {
+    return (
+        <p>Hello world</p>
+    );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
