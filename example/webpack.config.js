9b09b422092d3341078c7baed7e492de73a40cbf
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-08-02 16:52:06 +0200

    example: fix webpack to work with direct source code

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -15,11 +15,14 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
                     },
                 },
             },
         ],
+    },
+    resolve: {
+        extensions: ['.web.js', '.js'],
     },
     mode: 'development',
     plugins: [
