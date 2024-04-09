3d37ad4e558d002bd18ef9eceb5ec4d818c8ed65
Christoph Pader <chris@margelo.io>
2024-04-09 12:11:09 +0200

    update jestSetup config with new mocks

--- jestSetup.js
+++ jestSetup.js
@@ -1,7 +1,7 @@
 jest.mock('./lib/storage');
+jest.mock('./lib/storage/providers');
 jest.mock('./lib/storage/platforms/index.native', () => require('./lib/storage/__mocks__'));
-jest.mock('./lib/storage/platforms/index', () => require('./lib/storage/__mocks__'));
-jest.mock('./lib/storage/providers/IDBKeyValProvider', () => require('./lib/storage/__mocks__'));
+jest.mock('idb-keyval', () => require('./node_modules/idb-keyval/dist/mock'));
 
 jest.mock('react-native-device-info', () => ({getFreeDiskStorage: () => {}}));
 jest.mock('react-native-quick-sqlite', () => ({
